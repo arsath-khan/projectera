86e789f0a18227df0c0c5f390be2d1befc28bb5b
arsath-khan <69092255+arsath-khan@users.noreply.github.com>
2020-08-18 16:08:44 +0530

    Update script1.js

--- ajax/script1.js
+++ ajax/script1.js
@@ -6,7 +6,7 @@ function myfunction()
    xhttp.onreadystatechange=function(){
 	   if(xhttp.readyState==4 && xhttp.status ==200)
 		{
-         console.log(xhttp.responseText);
+         document.getElementById("12").innerHTML=xhttp.responseText;
 		}
    };
  xhttp.open("GET","server.txt",true);
